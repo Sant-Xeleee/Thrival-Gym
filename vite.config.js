441027e919efd469fca301ae0db72aeee2b7af06
Santiago Emi Perez <santi.perezcervera@gmail.com>
2026-03-30 00:05:45 -0600

    Segunda subida

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://sant-xeleee.github.io/Thrival-Gym/',
   plugins: [
     vue(),
     vueDevTools(),
